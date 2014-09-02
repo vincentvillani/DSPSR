0967877a03ed30c568100d931d67139a58e958e4
Vincent Villani <villanivincent@gmail.com>
2014-09-02 16:29:03 +1000

    Outputting covariance matrix results

--- Signal/Pulsar/CovarianceMatrixEngineCUDA.cu
+++ Signal/Pulsar/CovarianceMatrixEngineCUDA.cu
@@ -26,8 +26,9 @@ void computeCovarianceMatrixCUDAEngine(float* d_resultVector, unsigned int resul
 
 	printf("Launching Mean Kernel with gridDim: %d, blockDim: %d\n", meanGridDim, meanBlockDim);
 
-	printf("hit zero: %d\n", h_hits[0]);
-	printf("amp zero: %d\n", h_amps[0]);
+	printf("amp zero: %d\n", h_amps[1]);
+	printf("hit zero: %d\n", h_hits[1]);
+
 
 	meanStokesKernel<<< meanGridDim, meanBlockDim >>>(d_amps, ampsLength, d_hits, stokesLength);
 
